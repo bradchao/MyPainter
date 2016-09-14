2f76caa3ec62d4e53c52185cf42258a2015047b6
Brad Chao <brad@brad.tw>
2016-09-14 10:00:26 +0800

    v7

--- app/src/main/java/tw/brad/mypainter/MyView.java
+++ app/src/main/java/tw/brad/mypainter/MyView.java
@@ -15,11 +15,11 @@ import java.util.LinkedList;
  * Created by brad on 2016/9/13.
  */
 public class MyView extends View {
-    private LinkedList<HashMap<String,Float>> line;
+    private LinkedList<LinkedList<HashMap<String,Float>>> lines;
 
     public MyView(Context context, AttributeSet attrs) {
         super(context, attrs);
-        line = new LinkedList<>();
+        lines = new LinkedList<>();
     }
 
     @Override
@@ -28,9 +28,11 @@ public class MyView extends View {
         Paint p = new Paint();
         p.setColor(Color.BLUE);
         p.setStrokeWidth(4);
-        for (int i=1; i<line.size(); i++) {
-            canvas.drawLine(line.get(i-1).get("x"), line.get(i-1).get("y"),
-                    line.get(i).get("x"), line.get(i).get("y"), p);
+        for (LinkedList<HashMap<String,Float>> line:lines) {
+            for (int i = 1; i < line.size(); i++) {
+                canvas.drawLine(line.get(i - 1).get("x"), line.get(i - 1).get("y"),
+                        line.get(i).get("x"), line.get(i).get("y"), p);
+            }
         }
     }
 
@@ -46,17 +48,19 @@ public class MyView extends View {
     }
 
     private void doTouchDown(float x, float y){
-        HashMap<String,Float> point =
-                new HashMap<>();
-        point.put("x", x); point.put("y", y);
-        line.add(point);
-        invalidate();
+        LinkedList<HashMap<String,Float>> line =
+                new LinkedList<>();
+        lines.add(line);
+        addPoint(x,y);
     }
     private void doTouchMove(float x, float y){
+        addPoint(x,y);
+    }
+    private void addPoint(float x, float y){
         HashMap<String,Float> point =
                 new HashMap<>();
         point.put("x", x); point.put("y", y);
-        line.add(point);
+        lines.getLast().add(point);
         invalidate();
     }
 
